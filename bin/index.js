cb02bf97f0368e39ef858f396abbb58b9bf2c501
Donald Geddes <hi@donaldgeddes.ca>
2022-02-08 20:54:04 -0800

    Create new directory from input; add commenting

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-import { $, question, fs, path } from 'zx'
+import { $, question, fs, path, cd } from 'zx'
 
 import {
   GITHUB_USER_NAME,
@@ -14,30 +14,41 @@ import { defaultTest } from '@hbauer/init-project/src/default-test.js'
 
 import { buildPackageJson } from '@hbauer/init-project/src/build-package-json.js'
 
+const cwd = process.cwd()
+const pathTo = to => path.join(cwd, to)
+
 console.log(`
 ========================
   Create a new package 
 ========================
 `)
 
-const cwd = process.cwd()
-const pathTo = to => path.join(cwd, to)
-
-const name = await question(' => Package name: ')
-const respositoryName = name.startsWith(NPM_SCOPE)
-  ? name.slice(NPM_SCOPE.length + 1)
-  : name
+// Construct dynamic package.json fields
+const packageName = await question(' => Package name: ')
+const respositoryName = packageName.startsWith(NPM_SCOPE)
+  ? packageName.slice(NPM_SCOPE.length + 1)
+  : packageName
 const repository = `https://github.com/${GITHUB_USER_NAME}/${respositoryName}.git`
-const packageJson = buildPackageJson({ name, repository })
 
+// Build package.json
+const packageJson = buildPackageJson({ name: packageName, repository })
+
+// Create new directory
+await $`mkdir ${packageName}`
+await cd(packageName)
+
+// Create empty index.js
 await $`mkdir src`
 await $`touch src/index.js`
 
+// Initialize git
 await $`git init`
 
+// Write default files
 fs.writeFileSync(pathTo('package.json'), JSON.stringify(packageJson, null, 2))
 fs.writeFileSync(pathTo('.gitignore'), defaultGitignore)
 fs.writeFileSync(pathTo('rollup.config.js'), defaultRollupConfig)
 fs.writeFileSync(pathTo('src/test.js'), defaultTest)
 
+// Finish up
 $`yarn add -D ${defaultModules}`
